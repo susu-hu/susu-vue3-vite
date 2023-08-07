dabd94d1656b148a6fba1f8ef0531aa6e02323ee
susu <1628469970@qq.com>
2023-08-07 15:24:08 +0800

    fix:添加echarts的off事件

--- web/src/views/echarts/components/echart.vue
+++ web/src/views/echarts/components/echart.vue
@@ -1,3 +1,11 @@
+<!--
+ * @Author: susu 1628469970@qq.com
+ * @Date: 2023-08-07 11:50:13
+ * @LastEditors: susu 1628469970@qq.com
+ * @LastEditTime: 2023-08-07 15:23:45
+ * @FilePath: \web\src\views\echarts\components\echart.vue
+ * @Description: 通用echarts封装
+-->
 <template>
   <div ref="Chart" class="chart" :style="{ '--w': width, '--h': height }"></div>
 </template>
@@ -41,7 +49,15 @@ const renderChart = () => {
 // 页面卸载
 onBeforeUnmount(() => {
   window.removeEventListener("resize", resizeChart);
-  mChart.dispose(); //销毁echarts实例
+  // 移出事件
+  if (mChart.off) {
+    // ECharts 的 "off" 方法可以用于取消绑定所有类型的事件。如果要取消绑定所有事件处理函数，可以简单地调用 "off" 方法，而不传递任何参数
+    // 这将取消绑定通过 "on" 方法绑定的所有事件处理函数，包括内置的事件类型（如鼠标点击、鼠标移动等），以及自定义的事件类型
+    // Eg:mChart.off("click"); mChart.off("mouseover"); mChart.off("mouseout"); mChart.off("mousemove"); mChart.off("dataZoom"); mChart.off("legendselectchanged");
+    mChart.off();
+  }
+  mChart && mChart.dispose(); //销毁echarts实例
+  mChart = null;
 });
 </script>
 <style lang="less" scoped>
